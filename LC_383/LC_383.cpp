e07807e42cd3fbae3ec4fba3b60040fd4030676f
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-07-04 11:57:21 +0300

    Добавьте файлы проекта.

---
+++ LC_383/LC_383.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+bool canConstruct(std::string ransomNote, std::string magazine) {
+    std::unordered_map<char, int> blocknote;
+
+    for (char c : magazine) {
+        blocknote[c]++;
+    }
+
+    for (char c : ransomNote) {
+        if (blocknote[c] > 0) {
+             blocknote[c]--;
+        }
+        else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::string ransomNote = "ss";
+    std::string magazine = "rrtsyt";
+    return canConstruct(ransomNote, magazine);
+}
+
